e80b2931a270a11fc9e011276492b7563dc78a2a
dr-erych <dave@satoshiengineering.com>
2024-01-29 23:46:42 +0100

    change bluetooth install to use bluez-alsa

--- install-bluetooth.sh
+++ install-bluetooth.sh
@@ -4,65 +4,29 @@ read -p "Do you want to install Bluetooth Audio (ALSA)? [y/N] " REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then return; fi
 
 # Bluetooth Audio ALSA Backend (bluez-alsa-utils)
-sudo apt update
-sudo apt install -y --no-install-recommends bluez-tools bluez-alsa-utils
 
-# Bluetooth settings
-sudo tee /etc/bluetooth/main.conf >/dev/null <<'EOF'
-[General]
-Class = 0x200414
-DiscoverableTimeout = 0
-
-[Policy]
-AutoEnable=true
-EOF
-
-# Bluetooth Agent
-sudo tee /etc/systemd/system/bt-agent@.service >/dev/null <<'EOF'
-[Unit]
-Description=Bluetooth Agent
-Requires=bluetooth.service
-After=bluetooth.service
-
-[Service]
-ExecStartPre=/usr/bin/bluetoothctl discoverable on
-ExecStartPre=/bin/hciconfig %I piscan
-ExecStartPre=/bin/hciconfig %I sspmode 1
-ExecStart=/usr/bin/bt-agent --capability=NoInputNoOutput
-RestartSec=5
-Restart=always
-KillSignal=SIGUSR1
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable bt-agent@hci0.service
-
-
-# Bluetooth udev script
-sudo tee /usr/local/bin/bluetooth-udev >/dev/null <<'EOF'
-#!/bin/bash
-if [[ ! $NAME =~ ^\"([0-9A-F]{2}[:-]){5}([0-9A-F]{2})\"$ ]]; then exit 0; fi
-
-action=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
-
-if [ "$action" = "add" ]; then
-    bluetoothctl discoverable off
-    # disconnect wifi to prevent dropouts
-    #ifconfig wlan0 down &
+if [[ -z "$TMP_DIR" ]]; then
+    TMP_DIR=$(mktemp -d)
 fi
 
-if [ "$action" = "remove" ]; then
-    # reenable wifi
-    #ifconfig wlan0 up &
-    bluetoothctl discoverable on
-fi
-EOF
-sudo chmod 755 /usr/local/bin/bluetooth-udev
+cd $TMP_DIR
 
-sudo tee /etc/udev/rules.d/99-bluetooth-udev.rules >/dev/null <<'EOF'
-SUBSYSTEM=="input", GROUP="input", MODE="0660"
-KERNEL=="input[0-9]*", RUN+="/usr/local/bin/bluetooth-udev"
-EOF
+wget -O bluez-alsa-master.zip https://github.com/arkq/bluez-alsa/archive/refs/heads/master.zip
+unzip bluez-alsa-master.zip
+cd bluez-alsa-master
+
+sudo apt install automake build-essential libtool pkg-config python3-docutils libasound2-dev libbluetooth-dev libdbus-1-dev libglib2.0-dev libsbc-dev
+
+autoreconf --install --force
+
+mkdir build
+cd build
+
+../configure --enable-ofono --enable-debug
+
+make
+sudo make install
+
+
+# TODO automate discovery of devices and create autopairing config
+# https://gist.github.com/fagnercarvalho/2755eaa492a8aa27081e0e0fe7780d14
